44c6f98a6a3ed761c2a58e30d4b51a74002e3a10
danibig5566 <156295329+danibig5566@users.noreply.github.com>
2024-09-18 19:07:23 -0500

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template>  
+  <div>
+    <header class="encabezado">
+      <h1><center>Pokemon Go</center></h1>
+    </header>
+
+    <main>
+      <div class="pokemon" v-for="pokemon in pokemones" :key="pokemon.id">
+        <img :src="pokemon.imagen" :alt="pokemon.nombre" />
+        <h2 >{{ pokemon.nombre }}</h2>
+        <span :style="{'background-color':pokemon.type_color }">{{ pokemon.type_name }}</span>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      pokemones: [] 
+    };
+  },
+  mounted() {
+    this.getPokemones(); 
+  },
+  methods: {
+    getPokemones() {
+      axios
+        .get('https://cobuses.com.co/APIV2/model/pokemon.php?dato=getallpokemon')
+        .then((response) => {
+          this.pokemones = response.data.map(pokemon => ({
+            ...pokemon,
+          }));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+main {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 20px;
+}
+
+.pokemon {
+  margin: 10px;
+  padding: 10px;
+  border-radius: 8px;
+  width: 150px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #f3f3f3;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.pokemon img {
+  max-width: 100%;
+  height: auto;
+  border-radius: 8px;
+}
+
+.pokemon h2 {
+  margin-top: 10px;
+  font-size: 16px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+ import App from './App.vue'
+  import router from './router'
+  import axios from "axios"; 
+  import VueAxios from 'vue-axios'; 
+  const app = createApp(App).use(router); 
+  app.use(VueAxios, axios); 
+   app.mount("#app") 
+    export {app};  
 
 createApp(App).mount('#app')
